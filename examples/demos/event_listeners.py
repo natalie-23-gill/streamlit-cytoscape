1af5482c9ba2480bd3e78ffa33cbd8134bf689d1
Natalie Gill <natalie.gill@gladstone.ucsf.edu>
2025-12-08 14:34:14 -0800

    missed one

--- examples/demos/event_listeners.py
+++ examples/demos/event_listeners.py
@@ -16,7 +16,7 @@ st.error(
     With node actions feature added, the use case for this feature is no
     longer clear and may be removed in the future. If you have a use case
     for it please
-    share via [Github](https://github.com/natalie-23-gill/st-cytoscape).
+    share via [Github](https://github.com/natalie-23-gill/streamlit-cytoscape).
     """,
     icon="🚨",
 )
